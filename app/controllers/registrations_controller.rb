756251c366ff2d72a976970728b1478444432c23
Michał <ul0253566@edu.uni.lodz.pl>
2023-04-05 22:30:16 +0200

    edit profile form, some translations, views and nav

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,28 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
-  protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :password])
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:notice] = "Konto zostało zaktualizowane."
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, length: { minimum: 3 }
+  validates :phone, length: { is: 9 }
+
+
   enum role: [:client, :employee]
   after_initialize :set_default_role, :if => :new_record?
 
